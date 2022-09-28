53d1b58630b522b3d87d4d389c4c9bfe2ae8fff5
Tuan <tuan.nguyen3@citek.vn>
2022-09-28 17:46:49 +0700

    setup to deploy to heroku

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const hbs= require('hbs')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-
 const app = express()
+const port = process.env.PORT || 3000
 
 // define path for Express config
 const publicDirectory = path.join(__dirname, '../public')
@@ -106,6 +106,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 })
